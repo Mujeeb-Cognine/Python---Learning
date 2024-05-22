c7819fcf21e073b5165612b009fcfacdd0186f70
Mujeeb <mujeeb.rahaman@cognine.com>
2024-05-22 18:21:41 +0530

    Day printing, strmg manuplulation, python input and variables and band name generator project

---
+++ Day 1/1.2 - String Manipulation and Code Intelligence.py
@@ -0,0 +1,32 @@
+# To print in the next line
+print('Hello world!\nHello world!\nHello world!')
+
+# Concatenate strings
+# If there is space in any of the line we will get indentation errors
+# Enable the code intelligence to avoid the mistakes, this will show the errors
+print("Hello" + " " + "Mujeeb")
+
+# Look at the code in the code editor on the left. There are errors on all 4 lines of code. Fix the code so that it
+# runs without errors.
+
+# Example Output
+# Day 1 - String Manipulation
+# String Concatenation is done with the  sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+# Hint
+# The output in your program should match the example output shown above exactly, character for character, even spaces
+# and symbols should be identical, otherwise the tests won't pass!
+
+
+# Fix the code below 👇
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
---
+++ Day 1/1.3 - The Python Input Function.py
@@ -0,0 +1,86 @@
+# To ask question in the terminal we need to use input function
+
+input("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+# Use Thonny tool to check how the code works step by step
+
+#  What ever we are writing in the # this will be considered as comment in the python
+
+print(input())
+print(input())
+print(input())
+
+# Inputs
+
+# Jane
+# Ben
+# Tom
+
+
+# If a student didn't complete the coding exercise, then the extra tests should catch this.
+#
+# For example in this case 6 is the correct answer for the visible inputs. But behind the scenes when you submit the
+# code we are also passing in 5 and 5.
+#
+# 5 x 5 = 25
+#
+# while
+#
+# 2 x 3 = 6
+#
+# So the only way for the student to pass all the tests in the evaluation is to write the correct code.
+#
+# This is how we ensure you're doing the right things and test your understanding of the programming concepts. It's
+# like having a teacher next to you checking your code.
+
+num1 = int(input())
+num2 = int(input())
+
+print(6)
+
+# Inputs
+
+# 2
+# 3
+
+# Correct Code
+print(num1 * num2)
+
+# Try it yourself. Print the input text into the output area.
+
+# Input
+
+# Change this
+
+print(input())
+
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+# Write a program that calculates and outputs the number of characters in any name. The automated tests will try out
+# lots of different names as the input. Your code should work for any name. Your code should only output the number,
+# no other text is needed.
+#
+# Hint
+# Remember, you can use len() around any piece of text to calculate the number of characters.
+#
+# e.g. https://www.google.com/search?q=how+to+get+the+length+of+a+string+in+python+stack+overflow
+#
+# Important
+# Don't add prompt text to the input() function.
+#
+# e.g. use
+#
+# ✅ name = input()
+#
+# don't use:
+#
+# ❌ name = input("What's your name?")
+
+# Inputs
+# Mujeeb
+# James
+# Phillips
+# Ernest Hemingway
