64f3bc36dbe1655704d6a95c7f81ca349f763b9e
Mujeeb <mujeeb.rahaman@cognine.com>
2024-05-28 13:19:28 +0530

    Changed the names of the folders.

--- Day 1 - Working with Variables in Python to Manage Data/1.2 - String Manipulation and Code Intelligence.py
+++ Day 1 - Working with Variables in Python to Manage Data/1.2 - String Manipulation and Code Intelligence.py
@@ -10,7 +10,7 @@ print("Hello" + " " + "Mujeeb")
 # runs without errors.
 
 # Example Output
-# Day 1 - String Manipulation
+# Day 1 - Working with Variables in Python to Manage Data - String Manipulation
 # String Concatenation is done with the  sign.
 # e.g. print("Hello " + "world")
 # New lines can be created with a backslash and n.
@@ -21,12 +21,12 @@ print("Hello" + " " + "Mujeeb")
 
 
 # Fix the code below 👇
-# print(Day 1 - String Manipulation")
+# print(Day 1 - Working with Variables in Python to Manage Data - String Manipulation")
 # print("String Concatenation is done with the "+" sign.")
 #   print('e.g. print("Hello " + "world")')
 # print(("New lines can be created with a backslash and n.")
 
-print("Day 1 - String Manipulation")
+print("Day 1 - Working with Variables in Python to Manage Data - String Manipulation")
 print("String Concatenation is done with the " + " sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
